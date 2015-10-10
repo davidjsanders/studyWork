3f443f6042d21eeb9326f2d6b375536a59e191c9
dsandersAzure <dsanders_can@outlook.com>
2015-10-10 12:35:35 -0400

    Updating prototyping

--- prototyping/early-ws-work-sep-2015/ws/runDockerDeamon.sh
+++ prototyping/early-ws-work-sep-2015/ws/runDockerDeamon.sh
@@ -2,4 +2,5 @@
 portToUse=$1
 if [ "$1x" == "x" ]; then portToUse='5000'; fi
 echo "Starting service on port $portToUse"
-docker run -p $portToUse:5000 -P -d dsanders/phonews /bin/bash -c "/phonews/phoneWS.py"
+#docker run -p $portToUse:5000 -P -d dsanders/phonews /bin/bash -c "/phonews/phoneWS.py"
+docker run -p $portToUse:5000 -P -d dsanders/phonews /bin/bash -c "/phonews/startup.sh"
