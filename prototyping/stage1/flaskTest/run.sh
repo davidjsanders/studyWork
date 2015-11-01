61e1d52c7dcabc14b28c6054005ab970f265f7df
David J. Sanders <dsanders_can@outlook.com>
2015-11-01 20:36:47 +0000

    updates on Azure

--- prototyping/stage1/flaskTest/run.sh
+++ prototyping/stage1/flaskTest/run.sh
@@ -4,6 +4,6 @@ portToUse=$1
 if [ "$1x" == "x" ]; then portToUse='5000'; fi
 echo "Starting service on port $portToUse"
 docker run -p $portToUse:5000 -P --name flasktest \
-    -v /home/dasander/Documents/studyWork/prototyping/stage1/flaskTest/datavol:/datavol \
+    -v /home/dasander/studyWork/prototyping/stage1/flaskTest/datavol:/datavol \
     -d dsanders/flasktest /bin/bash -c /gendev/startup.sh
 
