9a2d4564eb8e28b1d0b6f994fd375d852dbfc2a0
dsandersAzure <dsanders_can@outlook.com>
2015-11-03 20:32:58 -0500

    testing

---
+++ prototyping/stage1/notifications/resources/Config.py
@@ -0,0 +1,18 @@
+"""
+This module contains key configuration variables used throughout
+the package.
+"""
+
+notificationList = []
+locked = False
+unlock_code = 1234
+
+output_messages = {
+    'message':'Hello World!'
+}
+authorized_ids = ['dasander','diane','david','bob']
+database_connection = '/datavol/config.db'
+message = {
+    'default':'This is the default message'
+}
+testing_enabled = False
--- prototyping/stage1/notifications/run.sh
+++ prototyping/stage1/notifications/run.sh
@@ -3,7 +3,7 @@ echo - n "Directory: "; pwd
 portToUse=$1
 if [ "$1x" == "x" ]; then portToUse='5000'; fi
 echo "Starting service on port $portToUse"
-docker run -p $portToUse:5000 -P --name flasktest \
+docker run -p $portToUse:5000 -P --name notifications \
     -v /home/dasander/Documents/studyWork/prototyping/stage1/flaskTest/datavol:/datavol \
-    -d dsanders/flasktest /bin/bash -c /gendev/startup.sh
+    -d dsanders/notifications /bin/bash -c /gendev/startup.sh
 
