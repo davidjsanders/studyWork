9144ca7aa19043b8dd30b3bd04e41b5aca1b467b
dsandersAzure <dsanders_can@outlook.com>
2015-11-04 19:32:04 -0500

    cleaning up

--- prototyping/stage1/notes/bin/run.sh
+++ prototyping/stage1/notes/bin/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo - n "Directory: "; pwd
 portToUse=$1
 if [ "$1x" == "x" ]; then portToUse='5000'; fi
-echo "Starting service on port $portToUse"
-docker run -p $portToUse:5000 -P --name notes \
+echo -n "Starting port $portToUse >> "
+docker run -p $portToUse:5000 -P --name notes$portToUse \
     -d dsanders/notes /bin/bash -c /notes/startup.sh
 #    -v /home/dasander/Documents/studyWork/prototyping/stage1/flaskTest/datavol:/datavol \
 
--- prototyping/stage1/notes/stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker stop notes
-docker rm -f notes
