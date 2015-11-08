f5c733562adce592f96c7c9066c406c868ef12fc
dsandersAzure <dsanders_can@outlook.com>
2015-11-08 14:42:45 -0500

    SQLite3 Updates

--- stage1/notesuwsgi/bin/run.sh
+++ stage1/notesuwsgi/bin/run.sh
@@ -2,6 +2,6 @@
 portToUse=$1
 if [ "$1x" == "x" ]; then portToUse='5000'; fi
 echo -n "Starting port $portToUse >> "
-docker run -p $portToUse:5000 -p 4082:80 -P --name notesuwsgi$portToUse \
+docker run -p $portToUse:80 -P --name notesuwsgi$portToUse \
     -v ~/Documents/studyWork/stage1/notesuwsgi/datavol:/notes/datavol \
     -d dsanders/notesuwsgi /bin/bash -c /notes/startup.sh \
--- stage1/notesuwsgi/startup.sh
+++ stage1/notesuwsgi/startup.sh
@@ -1,3 +1,4 @@
-#service nginx start
-/flask/bin/uwsgi --http :5000 --wsgi-file runserver.py --callable app --master --processes 4 --threads 2
+service nginx start
+/flask/bin/uwsgi notesuwsgi.ini
+#/flask/bin/uwsgi --http :5000 --wsgi-file runserver.py --callable app --master --processes 4 --threads 2
 #/flask/bin/python3 /notes/runserver.py
