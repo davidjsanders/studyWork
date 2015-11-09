833bf6cd0d8ea8c3d84bc6f4e0faf2a901cd021e
Ubuntu <ubuntu@dasanderDocker02.dasanderDocker02.b8.internal.cloudapp.net>
2015-11-09 22:44:16 +0000

    Fixed notes_final bin/run.sh

--- stage1/notes_final/bin/run.sh
+++ stage1/notes_final/bin/run.sh
@@ -4,5 +4,5 @@ if [ "$1x" == "x" ]; then portToUse='5000'; fi
 echo -n "Starting port $portToUse >> "
 docker run -p $portToUse:$portToUse --name notesfinal$portToUse \
     -e portToUse=$portToUse \
-    -v ~/Documents/studyWork/stage1/notes_final/datavol:/notes/datavol \
+    -v $PWD/datavol:/notes/datavol \
     -d dsanders/notesfinal /bin/bash -c '/notes/startup.sh ${portToUse}'
