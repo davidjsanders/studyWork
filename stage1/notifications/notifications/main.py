813aa7ef09e151619ea5d7d4d36d1fd0d36443db
dsandersAzure <dsanders_can@outlook.com>
2015-11-24 09:26:39 -0500

    Notifications

--- stage1/notifications/notifications/main.py
+++ stage1/notifications/notifications/main.py
@@ -7,12 +7,14 @@ import json
 import sqlite3
 
 class Notification_Boundary_All(Resource):
+    database_name = 'datavol/notifications.db'
+
     def get(self):
         try:
             return_list = []
             return_string = ''
 
-            db_connection = sqlite3.connect('datavol/notifications.db')
+            db_connection = sqlite3.connect(self.database_name)
             db_cursor = db_connection.cursor()
             db_cursor.execute(
                 'select key, value from notifications'
@@ -42,28 +44,33 @@ class Notification_Boundary_All(Resource):
 
         try:
             raw_json = reqparse.request.get_data().decode('utf-8')
-#            json_data = json.loads(raw_json)
             note = Notification()
             note.load(raw_json)
 
-            db_connection = sqlite3.connect('datavol/notifications.db')
+            db_connection = sqlite3.connect(self.database_name)
             db_cursor = db_connection.cursor()
+            db_cursor.execute('select ifnull(max(key),0) from notifications')
+            db_records = db_cursor.fetchall()
+            note.identifier = db_records[0][0] + 1
             db_cursor.execute( \
-                'insert into notifications '+ \
+                'insert into notifications (key, value) '+ \
                 'values (?, ?)', \
-                (note.identifier, raw_json) \
+                (note.identifier, note.dump()) \
             )
             db_connection.commit()
-            return_list = str(note)
+            return_list = note.dump()
         except IndexError:
             abort(404)
         except exceptions.ValidationError as ve:
             return_status = 400
             return_list = {'error':'Data not loaded. '+\
-                str(ve.message), 'status':return_status}
+                str(ve.message)}
+            abort(return_status, message=return_list)
         except Exception as e:
             return_status = 400
-            return_list = {'error':repr(e), 'status':return_status}
+            return_list = {'error':'Data not loaded. '+\
+                repr(e)}
+            abort(return_status, message=return_list)
         finally:
             if database_opened:
                 db_cursor.close()
@@ -72,9 +79,102 @@ class Notification_Boundary_All(Resource):
         return return_list
 
 class Notification_Boundary_One(Resource):
-    def get(self, id):
-        return 'Hello World {0}'.format(id)
+    database_name = 'datavol/notifications.db'
 
+    def get(self, id):
+        try:
+            return_list = []
+            return_string = ''
+            database_opened = False
+            updated_data = False
+
+            db_connection = sqlite3.connect(self.database_name)
+            db_cursor = db_connection.cursor()
+            database_opened = True
+            db_cursor.execute(
+                'select key, value from notifications where key = ?',
+                (id,) \
+            )
+            db_records = db_cursor.fetchall()
+            for db_row in db_records:
+                note = Notification()
+                note.load(db_row[1])
+                if not note.sensitivity == 'high':
+                    return_list.append(note.dump())
+                    return_string += ' '+note.dump()
+        except exceptions.ValidationError as ve:
+            pass
+        except Exception as e:
+            return_status = 400
+            return_list = {'error':repr(e)}
+            abort(return_status, message=return_list)
+        finally:
+             db_cursor.close()
+             db_connection.close()
+        
+        return return_list
+
+    def put(self, id):
+        return_list = []
+        database_opened = False
+        cursor_opened = False
+        updated_data = False
+
+        try:
+            raw_json = reqparse.request.get_data().decode('utf-8')
+            note = Notification()
+            note.load(raw_json)
+
+            db_connection = sqlite3.connect(self.database_name)
+            database_opened = True
+            db_cursor = db_connection.cursor()
+            cursor_opened = True
+
+            db_cursor.execute( \
+                'update notifications '+ \
+                'set value = ? '+ \
+                'where key = ?', \
+                (raw_json, \
+                 id ) \
+            )
+
+            db_connection.commit()
+            return_list = note.dump()
+        except Exception as e:
+            return_status = 400
+            return_list = {'error':repr(e)}
+            abort(return_status, message=return_list)
+        finally:
+            if cursor_opened:
+                db_cursor.close()
+            if database_opened:
+                db_connection.close()
+        
+        return return_list
+
+    def delete(self, id):
+        try:
+            return_list = []
+            database_opened = False
+            updated_data = False
+
+            db_connection = sqlite3.connect(self.database_name)
+            db_cursor = db_connection.cursor()
+            db_cursor.execute(
+                'delete from notifications where key = ?', (id,)
+            )
+            db_connection.commit()
+            return_list = {'success':'done.'}
+        except Exception as e:
+            return_status = 400
+            return_list = {'error':repr(e)}
+            abort(return_status, message=return_list)
+        finally:
+            if database_opened:
+                db_cursor.close()
+                db_connection.close()
+        
+        return {'message': return_list}
 
 api.add_resource(Notification_Boundary_All, '/v1_00/notifications')
 api.add_resource(Notification_Boundary_One, '/v1_00/notifications/<int:id>')
