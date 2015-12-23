30691a8b8d7b7a53c2127aec42cde6306eb82d14
dsandersAzure <dsanders_can@outlook.com>
2015-12-22 20:30:02 -0500

    Fix server name and port details

--- stage1/notifications/bin/run.sh
+++ stage1/notifications/bin/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 portToUse=$1
-if [ "$1x" == "x" ]; then portToUse='5000'; fi
-echo -n "Starting port $portToUse >> "
+serverName=$2
+if [ "$1x" == "x" ]; then portToUse='5000'; echo "Defaulting to port 5000"; fi
+if [ "$2x" == "x" ]; then serverName='localhost'; echo "Defaulting to localhost"; fi
+echo ""
+echo -n "Starting port $portToUse with server name $serverName>> "
 docker run -p $portToUse:$portToUse --name notifications$portToUse \
     --net=isolated_nw \
     -e portToUse=$portToUse \
-    -e serverName='dasanderUty01' \
+    -e serverName="$serverName" \
     -v $PWD/datavol:/notifications/datavol \
     -d dsanders/notifications /bin/bash -c /notifications/startup.sh \
