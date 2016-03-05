bb9f46be33763792d286766a859d56d4efef225d
dsandersAzure <dsanders_can@outlook.com>
2016-03-05 11:49:11 -0500

    Stage 3 - Added build.sh

---
+++ stage3/build.sh
@@ -0,0 +1,15 @@
+echo ""
+echo "Building LogViewer"
+cd ../LogViewer
+./build.sh
+echo ""
+echo "Building Monitor App"
+cd ../Monitor_App
+bin/build.sh
+echo ""
+echo "Building Phone"
+cd ../Phone
+bin/build.sh
+echo ""
+cd ..
+
