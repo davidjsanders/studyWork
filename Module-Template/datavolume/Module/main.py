9a0d52541b08fcbde7a9e1a5a0218fdc0b0410e2
dsandersAzure <dsanders_can@outlook.com>
2016-03-28 21:01:59 -0400

    Add Module-Template

--- Module-Template/datavolume/Module/main.py
+++
@@ -1,3 +0,0 @@
-from Module_Boundary import main
-from Module_Config_Boundary import main
-
--- Module-Template/datavolume/Module_Boundary/Sample_Boundary.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Resource, reqparse
-from Module.Sample_Control import sample_control_object
-
-class Sample_Boundary(Resource):
-    def get(self):
-        return sample_control_object.sample_request()
-
-
-
